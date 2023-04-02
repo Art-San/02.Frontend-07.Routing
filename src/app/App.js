2cd3b44e0c25a1b1dfc1147d8d666c3f5e10842d
Aleksander <asava@sibmail.com>
2023-04-02 13:55:04 +0700

    //Реализация Navbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,23 @@
 import NavBar from "./components/NavBar";
+import { Route } from 'react-router-dom'
+import Dashboard from "./components/Dashboard"
+import Posts from './components/Posts'
+import Login from './components/Login'
+import Home from './components/Home'
+
 
 function App() {
   return (
-    <div >
+     // Реализация Navbar
+    <div>
       <NavBar/>
-      <h1>APP</h1>
+      <h1>App</h1>
+      <Route path='/dashboard' component={Dashboard}/>
+      <Route path='/posts' component={Posts}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/' component={Home}/>
     </div>
-  )
+  );
 }
 
 export default App;
