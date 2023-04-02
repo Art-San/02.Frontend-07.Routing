4d1351f5c8ab765418cd16533d404f290b3d7e49
Aleksander <asava@sibmail.com>
2023-04-02 21:59:34 +0700

    Переадресация (редирект) 2 варианта

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import NavBar from "./components/NavBar"
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Dashboard from "./components/Dashboard"
 import Posts from './components/Posts'
 import Login from './components/Login'
 import Home from './components/Home'
 import Stats from "./components/Stats"
+import NotFound from "./components/NotFound"
+
+// {/* <Redirect to='/404' />  */}                   // Просто переадресация 
+// {/* <Redirect from="admin" to='/dashboard'/> */}  // Переадресация с несуществуещего (удаленного) адреса
+        
 
 function App() {
   return (
@@ -17,6 +22,9 @@ function App() {
         <Route path='/dashboard' component={Dashboard}/>
         <Route path='/login' component={Login}/>
         <Route path='/posts/:postId?' component={Posts} />
+        <Route path='/404' component={NotFound}/>
+        <Redirect from="/admin" to='/dashboard'/>
+        <Redirect to='/404' /> 
       </Switch>
     </div>
   )
