851e7e8cef58a03a951fa2c3022a3c5264f1e06b
Aleksander <asava@sibmail.com>
2023-04-03 12:52:15 +0700

    History. Программная навигация

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const Post = ({ id, posts }) => {
+const Post = ({ id, posts, history }) => {
     const getPostById = (id) => {
         return posts.find((post) => post.id.toString() === id)
     }
+    const hanleSave = () => {
+      // history.push('/posts') // Можем вернутся назад
+      history.replace('/posts') // не вернется назад после редактирования
+    }
     const post = getPostById(id)
   return (
+    <>
+    {' '}
     <h2>{post ? post.label : `Пост c этим id:${id} не найден`}</h2>
+    <button
+      onClick={hanleSave} >Сохранить</button>
+    </>
   )
 }
 
--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -1,31 +1,23 @@
-import React, { useEffect} from 'react'
+import React from 'react'
 import PostsList from "./PostsList"
 import Post from "./Post"
-import query from 'query-string'
-import _ from 'lodash'
 
-const Posts = ({ match, location }) => {
+
+const Posts = ({ match, history }) => {
   const posts = [ 
     {id: 1, label: 'post 1'},
     {id: 2, label: 'post 2'},
     {id: 3, label: 'post 3'},
   ]
-  const search = query.parse(location.search)
-  useEffect(() => {
-  console.log(search)
-  }, [search])
-  console.log('serch', search) // {count: '1', sortBy: 'newest'} // Query-параметры
+ 
   const postId = match.params.postId
-  const cropPosts = search
-      ? _(posts).slice(0).take(search.count).value()
-      : posts
-  console.log(cropPosts)
+
   return (
     <>
       {postId ? (
-            <Post posts={posts} id={postId} />
+            <Post posts={posts} id={postId} history={history}/>
         ) : (
-            <PostsList posts={cropPosts}/>
+            <PostsList posts={posts}/>
         )}
     </>
   )
